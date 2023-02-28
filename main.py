85789a31c9d661c73ae4077399275bf0294e5280
Supercip971 <supercyp971@gmail.com>
2023-02-28 08:46:55 -0400

    misc: ajout de commentiare

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import pygame, sys
 from pygame.locals import *
+
+# initialise le système de pygame
 pygame.init()
 
-RATIO = 16/9
 
+# La fenêtre aura 480 pixels de hauteur
+# Et la largeur aura 16/9 de la hauteur soit ~853 pixels
+RATIO = 16/9
 HEIGHT = 480
 WIDTH = int(RATIO*HEIGHT)
 
@@ -16,12 +20,27 @@ pygame.display.set_caption('Hello World!')
 
 def ucolor_to_pygame_color(r, g, b, a=1.0):
     return (int(r*255),int(g*255),int(b*255),int(a*255))
+
+
 while True: # main game loop
+    # Pour chaque évènements, si c'est un évènement "Exit" on quitte
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
+    # - JUSTE POUR TESTER -
+    # Pour chaque pixel de la fenêtre
+    # On met la couleur à: 
+    # rouge: x/WIDTH
+    # vert: y/HEIGHT
+    # bleu: x+y/(WIDTH+HEIGHT)
+    
+    # NOTE: On utilises des couleur qui ont des valeurs de 
+    # 0 à 1 contrairement a d'autres cas où on a besoins
+    # de couleurs de 0 à 255
     for x in range(WIDTH): 
         for y in range(HEIGHT): 
             DISPLAYSURF.set_at((x,y), ucolor_to_pygame_color(x/WIDTH, y/HEIGHT, (x+y)/(WIDTH*HEIGHT)))
+    # On met à jour la fenêtre.
     pygame.display.update()
+
