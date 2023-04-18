9c168f3339f9b27058b0ccea2d348f3c1fd20921
Supercip971 <supercyp971@gmail.com>
2023-04-18 14:59:50 -0400

    refactor: refactor vec3 code

--- sources/vector.py
+++ sources/vector.py
@@ -1,45 +1,65 @@
 import math
 
 
-class Vector:
-    def __init__(self, coords):
-        self.coords = coords
+def dot(v1, v2):
+    # v * v' = x * x' + y * y' + z * z'
+    return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z
+
+
+def cross(v1, v2):
+    # v x v' = (y * z' - z * y', z * x' - x * z', x * y' - y * x')
+    return Vec3(
+        v1.y * v2.z - v1.z * v2.y,
+        v1.z * v2.x - v1.x * v2.z,
+        v1.x * v2.y - v1.y * v2.x)
+
+
+class Vec3:
+
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
 
     def __str__(self):
-        return str(self.coords)
+        return str(f"({self.x}, {self.y}, {self.z})")
 
     def __add__(self, other):
-        return Vector([x + y for x, y in zip(self.coords, other.coords)])
+        # v_x = x + x'
+        # v_y = y + y'
+        # v_z = z + z'
+        return Vec3(self.x + other.x, self.y + other.y, self.z + other.z)
 
     def __sub__(self, other):
-        return Vector([x - y for x, y in zip(self.coords, other.coords)])
+        # v_x = x - x'
+        # v_y = y - y'
+        # v_z = z - z'
+        return Vec3(self.x - other.x, self.y - other.y, self.z - other.z)
 
     def __mul__(self, other):
-        if isinstance(other, Vector):
-            return sum([x * y for x, y in zip(self.coords, other.coords)])
-        elif isinstance(other, (int, float)):
-            return Vector([x * other for x in self.coords])
+        if isinstance(other, (float, int)):
+            return Vec3(self.x * other, self.y * other, self.z * other)
 
-    def norm(self):
-        return (sum([x ** 2 for x in self.coords])) ** 0.5
+    def length(self):
+        # |v| = sqrt(x^2 + y^2 + z^2)
+        return math.sqrt(self.x ** 2 + self.y ** 2 + self.z ** 2)
+
+    def normalize(self):
+        # v_x = x / |v|
+        # v_y = y / |v|
+        # v_z = z / |v|
+        return self * (1 / self.length())
 
     def distance(self, other):
-        return (sum([(x - y) ** 2 for x, y in zip(self.coords, other.coords)])) ** 0.5
+        # d = |v - v'|
+        return (self - other).length()
+
+    def dot(self, other):
+        # v * v' = x * x' + y * y' + z * z'
+        return self.x * other.x + self.y * other.y + self.z * other.z
 
     def angle(self, other):
-        dot_product = self * other
-        len_self = self.norm()
-        len_other = other.norm()
+        dot_product = dot(self, other)
+        len_self = self.length()
+        len_other = other.length()
         return math.acos(dot_product / (len_self * len_other))
-
-
-v1 = Vector([1, 2, 3])
-v2 = Vector([4, 5, 6])
-
-print(v1 + v2)  # Output: [5, 7, 9]
-print(v1 - v2)  # Output: [-3, -3, -3]
-print(v1 * v2)  # Output: 32
-print(v1 * 2)  # Output: [2, 4, 6]
-print(v1.norm())  # Output: 3.7416573867739413
-print(v1.distance(v2))  # Output: 5.196152422706632
-print(math.degrees(v1.angle(v2)))  # Output: 9.744562646538029
