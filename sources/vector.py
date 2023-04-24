b4dbb988f562719ebddeae0934038744e15fd328
Supercip971 <supercyp971@gmail.com>
2023-04-24 14:26:59 -0400

    fix: force les valeurs de vectors d'être des floats

--- sources/vector.py
+++ sources/vector.py
@@ -19,9 +19,9 @@ def cross(v1, v2):
 class Vec3:
 
     def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
+        self.x = float(x)
+        self.y = float(y)
+        self.z = float(z)
 
     def __str__(self):
         return str(f"({self.x}, {self.y}, {self.z})")
@@ -108,7 +108,7 @@ def refract(v, n, ior):
 
 
 def reflect(v, n):
-    return v - 2.0 * dot(v, n) * n
+    return v - 2.0 * dot(n, v) * n
 
 
 def schlick(angle, ior):
